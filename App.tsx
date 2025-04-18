4b429703d665048c6629511c569e1f683632ba69
Affana barry <affanabarry45@gmail.com>
2025-04-18 12:29:15 +0000

    Refactor App component to use FlatList for rendering book items

--- App.tsx
+++ App.tsx
@@ -1,19 +1,21 @@
 import './global.css';
 import { StatusBar } from 'expo-status-bar';
-import { View } from 'react-native';
+import { View, FlatList } from 'react-native';
 
 import books from './src/dummyBooks';
 import { BookListItem } from './src/components/BookListItem';
 
 export default function App() {
   return (
-    <View className='flex-1 bg-slate-800 justify-center p-4 gap-4'>
-      {/* Book row */}
-      <BookListItem book={books[0]} />
-      <BookListItem book={books[1]} />
-      <BookListItem book={books[2]} />
-      <BookListItem book={books[3]} />
-
+    <View className='flex-1 bg-slate-800 justify-center p-4 pt-16'>
+      <FlatList
+        data={books}
+        renderItem={({ item }) => <BookListItem book={item} />}
+        keyExtractor={(item) => item.id}
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ gap: 16 }}
+        className='flex-1'
+      />
       <StatusBar style="auto" />
     </View>
   );
